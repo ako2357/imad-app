c5e5fccfd20eb93192a0577ace6138c30362ecf8
AJAY KUMAR OTTA <30622971+ako2357@users.noreply.github.com>
2017-08-11 16:43:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
+app.get('/ui/article-one.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/', function (req, res) {
+app.get('/ui/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/ui/style.css', function (req, res) {
